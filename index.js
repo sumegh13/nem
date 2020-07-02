96babc85c1390b11880b59fb41a60b4e9e2a8c46
Sumegh Chanania <sumeghca@gmail.com>
2020-07-03 01:16:28 +0530

    Node HTTP Example 1

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-var rect = require('./rectangle');
+const http = require('http');
 
-function solveRect(l,b) {
-    console.log("Solving for rectangle with l = "
-                + l + " and b = " + b);
-    rect(l,b, (err,rectangle) => {
-        if (err) {
-	        console.log("ERROR: ", err.message);
-	    }
-        else {
-            console.log("The area of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.area());
-            console.log("The perimeter of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.perimeter());
-        }
-    });
-    console.log("This statement after the call to rect()");
-};
+const hostname = 'localhost';
+const port = 3000;
 
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
+const server = http.createServer((req, res) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>Hello, World!</h1></body></html>');
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
