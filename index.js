1ef6d53a07a66edc297c1bbe83063c574f810ec0
Sumegh Chanania <sumeghca@gmail.com>
2020-07-03 22:46:50 +0530

    Express Example

--- index.js
+++ index.js
@@ -1,51 +1,20 @@
-const http = require('http');
-
-const fs = require('fs');
-const path = require('path');
+const express = require('express'),
+     http = require('http');
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  console.log('Request for ' + req.url + ' by method ' + req.method);
+const app = express();
 
-  if (req.method == 'GET') {
-    var fileUrl;
-    if (req.url == '/') fileUrl = '/index.html';
-    else fileUrl = req.url;
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
-    var filePath = path.resolve('./public'+fileUrl);
-    const fileExt = path.extname(filePath);
-    if (fileExt == '.html') {
-      fs.exists(filePath, (exists) => {
-        if (!exists) {
-          res.statusCode = 404;
-          res.setHeader('Content-Type', 'text/html');
-          res.end('<html><body><h1>Error 404: ' + fileUrl + 
-                      ' not found</h1></body></html>');
-          return;
-        }
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        fs.createReadStream(filePath).pipe(res);
-        return;
-      });
-    }
-    else {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/html');
-      res.end('<html><body><h1>Error 404: ' + fileUrl + 
-              ' not a HTML file</h1></body></html>');
-      return;
-    }
-  }else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Error 404: ' + req.method + 
-            ' not supported</h1></body></html>');
-    return;
-}
-})
+});
+
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
